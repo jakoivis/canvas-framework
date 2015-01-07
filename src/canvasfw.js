1f8ece31f4e78abd6deeb833f155bf91dd89beae
Jarmo Koivisto <jarmo.kvst@gmail.com>
2015-01-07 22:30:30 +0200

    Remove animator stuff

--- src/canvasfw.js
+++ src/canvasfw.js
@@ -3,13 +3,9 @@
 // 	Graphic: require('./graphic.js'),
 // 	Timer: require('./timer.js'),
 // 	CanvasUtil: require('./canvasutil.js'),
-// 	Animator: require('./animator.js'),
-// 	Animation: require('./animation.js')
 // };
 
 window.Layer = require('./layer.js');
 window.Graphic = require('./graphic.js');
 window.Timer = require('./timer.js');
 window.CanvasUtil = require('./canvasutil.js');
-window.Animator = require('./animator.js');
-window.Animation = require('./animation.js');
