8ffe8b20c505775c93e950d040e3fe27ac565bd1
Jarmo Koivisto <jarmo.kvst@gmail.com>
2015-01-21 20:18:58 +0200

    Replace arguments with one object

--- src/transform.js
+++ src/transform.js
@@ -25,30 +25,39 @@ function Transform(imageData, context)
         return me;
     }
 
-    function transform(evaluatePixel, factor)
+    function transform(evaluatePixel, userParameters)
     {
         var imageDataPixels = imageData.data;
         var newImageData = context.createImageData(imageData);
         var newImageDataPixels = newImageData.data;
         var result = [];
         var length = newImageDataPixels.length;
+        var parameters = {
+            imageData: imageData
+        };
+
+        userParameters = userParameters || {};
+
+        for(var property in userParameters)
+        {
+            parameters[property] = userParameters[property];
+        }
 
         for (var i = 0; i < length; i += 4)
         {
-            result = evaluatePixel(
-                imageDataPixels[i],
-                imageDataPixels[i+1],
-                imageDataPixels[i+2],
-                imageDataPixels[i+3],
-                imageData,
-                (i%(imageData.width*4)/4),
-                Math.floor(i/(imageData.width*4)),
-                i);
+            parameters.r = imageDataPixels[i];
+            parameters.g = imageDataPixels[i+1];
+            parameters.b = imageDataPixels[i+2];
+            parameters.a = imageDataPixels[i+3];
+            parameters.x = (i % (imageData.width << 2)) >> 2;
+            parameters.y = Math.floor(i / (imageData.width << 2));
 
-            newImageDataPixels[i]   = result[0]; // r
-            newImageDataPixels[i+1] = result[1]; // g
-            newImageDataPixels[i+2] = result[2]; // b
-            newImageDataPixels[i+3] = result[3]; // a
+            result = evaluatePixel(parameters);
+
+            newImageDataPixels[i]   = result[0]; // R
+            newImageDataPixels[i+1] = result[1]; // G
+            newImageDataPixels[i+2] = result[2]; // B
+            newImageDataPixels[i+3] = result[3]; // A
         }
 
         return newImageData;
@@ -56,45 +65,54 @@ function Transform(imageData, context)
 
     Transform.sampleLinear = function(imageData, x, y)
     {
+        var data = imageData.data;
+        var index = y * (imageData.width << 2) + (x << 2);
+
         return [
-            imageData.data[y * imageData.width*4 + x*4],
-            imageData.data[y * imageData.width*4 + x*4+1],
-            imageData.data[y * imageData.width*4 + x*4+2],
-            imageData.data[y * imageData.width*4 + x*4+3]
+            data[index],
+            data[index+1],
+            data[index+2],
+            data[index+3]
         ];
     };
 
-    Transform.Invert = function(r, g, b, a)
+    Transform.Invert = function(p)
     {
-        return [255-r, 255-g, 255-b, a];
+        return [255-p.r, 255-p.g, 255-p.b, p.a];
     };
 
-    Transform.GrayScale = function(r, g, b, a)
+    Transform.GrayScale = function(p)
     {
-        var average = (r + g + b) /3;
-        return [average, average, average, a];
+        var average = (p.r + p.g + p.b) /3;
+        return [average, average, average, p.a];
     };
 
-    Transform.Rotate = function(r, g, b, a, imageData, x, y, i)
+    Transform.Alpha = function(p)
     {
-        var degree = 19;
-        var radian = (degree/180.0)*3.14159265;
-        var tx = Math.round(x*Math.cos(radian) - y*Math.sin(radian));
-        var ty = Math.round(x*Math.sin(radian) + y*Math.cos(radian));
-
-        return Transform.sampleLinear(imageData, tx, ty);
+        return [p.r, p.g, p.b, p.value];
     }
 
-    Transform.Swirl = function(r, g, b, a, imageData, x, y, i)
+    Transform.Rotate = function(p)
     {
-        var originX = 0;
-        var originY = 0;
-        var degree = 5;
-        var radius = 20;
+        var degree = p.degree;
+
+        var radian = (degree/180.0)*3.14159265;
+        var tx = Math.round(p.x*Math.cos(radian) - p.y*Math.sin(radian));
+        var ty = Math.round(p.x*Math.sin(radian) + p.y*Math.cos(radian));
+
+        return Transform.sampleLinear(p.imageData, tx, ty);
+    }
+
+    Transform.Swirl = function(p)
+    {
+        var originX = p.originX;
+        var originY = p.originY;
+        var degree = p.degree;
+        var radius = p.radius;
 
         // calculate distance of pixel from origin
-        var distanceX = x-originX;
-        var distanceY = y-originY;
+        var distanceX = p.x-originX;
+        var distanceY = p.y-originY;
         var distance = Math.sqrt(distanceX*distanceX + distanceY*distanceY);
 
         // radian is the greater the farther the pixel is from origin
@@ -105,9 +123,9 @@ function Transform(imageData, context)
         tx -= originX;
         ty -= originY;
 
-        tx = Math.round(x - tx);
-        ty = Math.round(y - ty);
+        tx = Math.round(p.x - tx);
+        ty = Math.round(p.y - ty);
 
-        return Transform.sampleLinear(imageData, tx, ty);
+        return Transform.sampleLinear(p.imageData, tx, ty);
     }
 }
